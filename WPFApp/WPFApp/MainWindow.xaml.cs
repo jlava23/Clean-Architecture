75fb768c4e27598e5533290896db016d24b1eabd
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2025-05-04 17:21:43 -0500

    WPFApp

---
+++ WPFApp/WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateTask();
+        }
+
+        void AddMessage(string message)
+        {
+            int CurrentThreadId = Thread.CurrentThread.ManagedThreadId;
+            this.Dispatcher.Invoke(() =>
+            {
+                Messages.Content +=
+                    $"Mensaje: {message}, " +
+                    $"Hilo Actual: {CurrentThreadId}\n";
+            });
+
+        }
+
+        void CreateTask()
+        {
+            Task T;
+            Action Code = new Action(ShowMessage);
+            T = new Task(Code);
+
+            Task T2 = new Task(delegate
+            {
+                MessageBox.Show("Ejecutando una tarea en un metodo anonimo");
+            }
+            );
+
+            Task T3 = new Task(
+                () => ShowMessage());
+
+            Task T4 = new Task(() => MessageBox.Show("Ejecutando la Tarea 4"));
+
+            Task T5 = new Task(() =>
+            {
+                DateTime CurrentDate = DateTime.Today;
+                DateTime StartDate = CurrentDate.AddDays(30);
+                MessageBox.Show($"Tarea 5. Fecha Calculada: {StartDate}");
+            }
+            );
+
+            Task T6 = new Task((message) =>
+            MessageBox.Show(message.ToString()), "Expresion Lambda con parametros");
+
+            Task T7 = new Task(() => AddMessage("Ejecutando la tarea."));
+            T7.Start();
+            AddMessage("En el hilo principal");
+        }
+
+        void ShowMessage()
+        {
+            MessageBox.Show("Ejecutando el método ShowMessage");
+        }
+    }
+}
